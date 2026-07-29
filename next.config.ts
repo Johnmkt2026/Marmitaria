4c59058def6e7bf01ec03def75a9824b016281a7
unknown <guigui.couto23@gmail.com>
2026-07-28 21:33:42 -0300

    fix: corrige service binding do Cloudflare

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,6 @@
 import type { NextConfig } from 'next';
-const nextConfig: NextConfig = { images: { remotePatterns: [{ protocol: 'https', hostname: '**' }] } };
+const nextConfig: NextConfig = {
+  output: 'standalone',
+  images: { remotePatterns: [{ protocol: 'https', hostname: '**' }] },
+};
 export default nextConfig;
